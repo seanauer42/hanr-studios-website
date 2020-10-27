5b44a8cc243e440bfed5d125a6f5e3b00e4e1a83
Sean Hanauer <yodaman40@gmail.com>
2020-10-27 12:04:00 -0400

    added some test to verify that was_published_recently worked

--- hanrstudios/polls/views.py
+++ hanrstudios/polls/views.py
@@ -73,6 +73,6 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
     #   Always return an HTTPResponseRedirect after successfully dealing
-    #   with POST data. This prvents data from being posted twice if a
+    #   with POST data. This prevents data from being posted twice if a
     #   user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
